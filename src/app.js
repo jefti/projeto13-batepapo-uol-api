552d165d7c8189be79c29e2bb23ae2b171e5e9b5
jefti <jeftimeira@gmail.com>
2023-06-30 23:13:16 -0300

    requisito 3 - método post participants

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
+import joi from 'joi';
+import dayjs from 'dayjs';
 
 //1. Criar o app
 const app = express();
@@ -19,28 +21,54 @@ mongoClient.connect()
     .then(()=> db = mongoClient.db())
     .catch((err)=> console.log(err.message));
 
+//3. Validação por Joi
 
-//3. Funções de Endpoint
-app.get("/",(req,res)=>{
-    res.send("Servidor Online!!!");
+const participantSchema = joi.object({
+    name: joi.string().required(),
+    lastStatus: joi.required()
 });
 
-    //3.1 funções Get
+
+//4. Funções de Endpoint
+app.get("/",(req,res)=>{
+    res.send("Teste");
+});
+
+    //4.1 funções Get
 app.get("/participants",(req,res)=>{
     res.send("Retornado lista de participantes");
 });
 
-app.get("/messages",(req,res)=>{
-    res.send("Retornado lista de mensagens");
+app.get("/messages",async (req,res)=>{
+    await db.collection("messages").find().toArray()
+    .then(mensagens => res.send(mensagens));
 });
 
-    //3.2 funções Post
-app.post("/participants",(req,res)=>{
-    res.send("Atualizado lista de participantes");
+    //4.2 funções Post
+app.post("/participants", async (req,res)=>{
+    const {name} = req.body;
+    try{
+        const resp = await db.collection("participants").findOne({name});
+        if (resp) return res.status(409).send("Nome já está em uso");
+        const obj = {name, lastStatus: Date.now()};
+        const mensagem = {from:name, to:'Todos',text:'entra na sala...',type:'status',time:dayjs().format('HH:mm:ss')}
+        const validation = participantSchema.validate(obj);
+        if (validation.error){
+            const erros = validation.error.details.map((detail)=> detail.message);
+            return res.status(422).send(erros);
+        }
+        await db.collection("participants").insertOne(obj);
+        await db.collection("messages").insertOne(mensagem);
+        return res.sendStatus(201);
+    } catch(err) {
+        return res.status(500).send(err.message);
+    }
+
+
 });
     
 app.post("/messages",(req,res)=>{
-    res.send("Atualizado lista de mensagens");
+
 });
 
 app.post("/status",(req,res)=>{
@@ -49,6 +77,12 @@ app.post("/status",(req,res)=>{
 
 
 
-//4. Ligar na porta específicada
+//5. Ligar na porta específicada
 const Port = 5000;
-app.listen(Port, ()=> console.log(`Servidor rodando na porta ${Port}`));
+const now = new Date;
+const nomeDia = ["Domingo", "Segunda", "Terça","Quarta","Quinta","Sexta","Sábado"];
+const nomeMes = ["Janeiro", "Fevereiro","Março","Abril","Maio","Junho","JUllho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
+app.listen(Port, ()=> {
+console.log(`Servidor rodando na porta ${Port} ${nomeMes[now.getMonth()]}`);
+console.log(dayjs().format('HH:mm:ss'));
+});
